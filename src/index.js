a9021ca66f6d43c407b88c007f448ee5fabdff0d
Isaac Suttell <isaac_suttell@playstation.sony.com>
2016-08-22 15:04:05 -0700

    Wrapped start up code in promise

--- src/index.js
+++ src/index.js
@@ -31,58 +31,61 @@ Ceres.prototype.Model = require(path.resolve(__dirname + '/rest/Model'));
  * @param  {Object} options
  */
 Ceres.prototype.load = function(options) {
-  // Bootstrap config
-  this.config = Setup.config(options);
-
-  if (this.config.env === 'production') {
-    // Save uncaught exceptions to their own file in production
-    winston.handleExceptions(new DailyRotateFile({
-      filename: this.config.folders.logs + '/exceptions.log',
-      tailable: true
-    }));
-  }
-
-  // Setup logging app
-  this.log = require('./setup/logs')(this.config);
-  // Setup internal framework logger so we can tell if its an app or framework erro
-  this.log._ceres = require('./setup/logs')(this.config, 'ceres');
-  this.log._ceres.silly('Logging setup');
-
-  // Bind the correct context
-  if (this.config.folders.middleware) {
-    this.config.middleware = Setup.directory(this.config.folders.middleware, this);
-    this.middleware = this.config.middleware;
-  }
-
-  /**
-   * Base Rest Controller API
-   *
-   * @type {Object}
-   */
-  this.Rest = {
-
-    /**
-     * Base Controller
-     *
-     * @type {Object}
-     */
-    Controller: require(path.resolve(__dirname + '/rest/Controller')),
-
-    /**
-     * Base Model
-     *
-     * @type {Object}
-     */
-    Model: require(path.resolve(__dirname + '/rest/models/' + this.config.db.type))
-  };
-
-  this.Pipeline = require(path.resolve(__dirname + '/render/Pipeline'));
-
-  // Bind the correct context
-  this.Pipeline.create = this.Pipeline.create.bind(this);
-  this.Rest.Model.extend = this.Rest.Model.extend.bind(this);
-
   return new Promise(function(resolve, reject){
+    // Bootstrap config
+    this.config = Setup.config(options);
+
+    if (this.config.env === 'production') {
+      // Save uncaught exceptions to their own file in production
+      winston.handleExceptions(new DailyRotateFile({
+        filename: this.config.folders.logs + '/exceptions.log',
+        tailable: true
+      }));
+    }
+
+    // Setup logging app
+    this.log = require('./setup/logs')(this.config);
+    // Setup internal framework logger so we can tell if its an app or framework erro
+    this.log._ceres = require('./setup/logs')(this.config, 'ceres');
+    this.log._ceres.silly('Logging configured');
+
+    // Bind the correct context
+    if (this.config.folders.middleware) {
+      this.config.middleware = Setup.directory(this.config.folders.middleware, this);
+      this.middleware = this.config.middleware;
+      this.log._ceres.silly('Middleware configured');
+    }
+
+    /**
+     * Base Rest Controller API
+     *
+     * @type {Object}
+     */
+    this.Rest = {
+
+      /**
+       * Base Controller
+       *
+       * @type {Object}
+       */
+      Controller: require(path.resolve(__dirname + '/rest/Controller')),
+
+      /**
+       * Base Model
+       *
+       * @type {Object}
+       */
+      Model: require(path.resolve(__dirname + '/rest/models/' + this.config.db.type))
+    };
+
+    this.Pipeline = require(path.resolve(__dirname + '/render/Pipeline'));
+
+    // Bind the correct context
+    this.Pipeline.create = this.Pipeline.create.bind(this);
+    this.Rest.Model.extend = this.Rest.Model.extend.bind(this);
+
+    this.log._ceres.silly('Rest module configured');
+
     this.log._ceres.silly('Setting up ' + this.config.db.type);
     var Database = require(__dirname + '/db')(this.config, this);
     return Database.then(function(db){
