a8ad887d6f780ee20e5b16c9d306a2d9670fcdda
Isaac Suttell <isaac_suttell@playstation.sony.com>
2016-09-27 08:49:27 -0700

    Fixed an error where errors where not being logged using winston

--- src/middleware/error.js
+++ src/middleware/error.js
@@ -1,6 +1,6 @@
 module.exports = function(ceres) {
   return function(err, req, res, next){
-    console.error(err);
+    ceres.log.error(err);
     if(ceres.config.env === 'production') {
       res.status(500).send('Internal Server Error').end();
     } else {
