6965a99d2a73ad74693c792bfe6c7b6fe6bb1725
Sepand Assadi <sepand.assadi@sony.com>
2017-04-04 21:16:16 -0700

    Updates the deepCopy function to copy dates object, adds tests

--- spec/lib/deepCopy_spec.js
+++ spec/lib/deepCopy_spec.js
@@ -34,6 +34,16 @@ describe('deepCopy', function(){
     expect(typeof result).toBe(typeof obj1);
   });
 
+  it('should return object type if input is a date', function() {
+    var obj1 = {
+      d: new Date()
+    };
+    var result = deepCopy(obj1);
+
+    expect(typeof result).toBe(typeof obj1);
+    expect(result.d).toEqual(obj1.d);
+  });
+
   it('should deeply copy an object', function() {
     var obj = {
       name: 'test',
@@ -102,5 +112,59 @@ describe('deepCopy', function(){
     expect(result[1].config.deepConfig.db).toBe(arr[1].config.deepConfig.db);
     expect(result[1].config.deepConfig.ssl).toBe(arr[1].config.deepConfig.ssl);
     expect(result[1].config.cache.type).toBe(arr[1].config.cache.type);
+  });
+
+  it('should copy the object correctly, not reference copy', function() {
+    // Making sure it's actually copy
+    // and not reference copy
+    var tree = {
+      'left'  : { 'left' : null, 'right' : null, 'data' : 3 },
+      'right' : null,
+      'data'  : 8
+    };
+
+    var result = deepCopy(tree);
+
+    // There are not a same object in memory
+    expect(result).not.toBe(tree);
+    expect(result.left).toEqual(tree.left);
+    expect(result.right).toEqual(tree.right);
+
+    // Now changing the source object,
+    // but result should not be changed
+    tree.right = tree.left;
+
+    // There are not a same object in memory
+    expect(result).not.toBe(tree);
+    expect(result.left).toEqual(tree.left);
+    expect(result.right).toEqual(null);
+    expect(result.right).not.toEqual(tree.right);
+  });
+
+  it('should copy the object correctly, not mutate', function() {
+    // Mutating a source object should have
+    // no effect on the result object
+    var a = {
+      test: '1'
+    };
+
+    var b = {
+      example: '2'
+    };
+
+    var test1 = deepCopy(a);
+    var test2 = deepCopy(b);
+
+    expect(a).not.toBe(b);
+    expect(test1).not.toBe(test2);
+
+    expect(typeof a.example).toBe('undefined');
+    expect(typeof test1.example).toBe('undefined');
+
+    expect(typeof b.test).toBe('undefined');
+    expect(typeof test2.test).toBe('undefined');
+
+    expect(test1.test).toEqual(a.test);
+    expect(test2.example).toEqual(b.example);
   });
 });
--- src/lib/deepCopy.js
+++ src/lib/deepCopy.js
@@ -5,14 +5,39 @@
  * @return    {Mixed}
  */
 module.exports = function deepCopy(obj) {
+  var copy;
+
   if (obj === null || typeof obj !== 'object') {
     return obj;
   }
-  var src = obj instanceof Array ? [] : {};
-  for (var key in obj) {
-    if (Object.prototype.hasOwnProperty.call(obj, key)) {
-      src[key] = deepCopy(obj[key]);
-    }
+
+  // Handle Date
+  if (obj instanceof Date) {
+    copy = new Date();
+    copy.setTime(obj.getTime());
+    return copy;
   }
-  return src;
+
+  // Handle Array
+  if (obj instanceof Array) {
+    copy = [];
+    for (var i = 0, len = obj.length; i < len; i++) {
+        copy[i] = deepCopy(obj[i]);
+    }
+    return copy;
+  }
+
+  // Handle Object
+  if (obj instanceof Object) {
+    copy = {};
+    for (var key in obj) {
+      if (obj.hasOwnProperty(key)) {
+        copy[key] = deepCopy(obj[key]);
+      }
+    }
+
+    return copy;
+  }
+
+  throw new Error('Unable to copy obj! Its type is not supported.');
 };
