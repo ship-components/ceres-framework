5a8bc38bfedb1bb52afede7f808021a21e1ea173
Isaac Suttell <isaac_suttell@playstation.sony.com>
2017-04-10 15:56:51 -0700

    Added unit tests for wrapRoute

---
+++ spec/controllers/warpRoute_spec.js
@@ -0,0 +1,118 @@
+'use strict';
+var wrapRoute = require('../../src/controllers/wrapRoute');
+var Promise = require('bluebird');
+
+describe('wrapRoute', function(){
+  var ceres;
+
+  beforeEach(function(){
+    ceres = {
+      log: {
+        _ceres: {
+          debug: function(){}
+        }
+      }
+    };
+  });
+
+	it('should return a function', function(){
+    var handler = function() {};
+    var ctx = {};
+    var result;
+    expect(function(){
+      result = wrapRoute(handler, ctx, ceres);
+    }).not.toThrow();
+    expect(typeof result).toBe('function');
+  });
+
+  it('should extend "this" with any custom properties', function(){
+    var result;
+    var handler = function() {
+      result = this;
+    };
+    var ctx = {
+      extend: true
+    };
+    expect(function(){
+      result = wrapRoute(handler, ctx, ceres);
+      result();
+    }).not.toThrow();
+    expect(result.extend).toBe(ctx.extend);
+  });
+
+  it('should assign "req", "res", "next" to "this" of the handler', function(){
+    var result;
+    var handler = function() {
+      result = this;
+    };
+    var ctx = {};
+
+    var req = {};
+    var res = {};
+    var next = function(){};
+    expect(function(){
+      result = wrapRoute(handler, ctx, ceres);
+      result(req, res, next);
+    }).not.toThrow();
+    expect(result.req).toBe(req);
+    expect(result.res).toBe(res);
+    expect(result.next).toBe(next);
+  });
+
+  it('should call the "next" handler if theres an error', function(){
+    var handler = function() {
+      throw new Error('ERROR');
+    };
+    var ctx = {};
+    var next = jasmine.createSpy();
+    var fn = wrapRoute(handler, ctx, ceres);
+    fn({}, {}, next);
+    expect(next).toHaveBeenCalled();
+  });
+
+  it('should automatically "send" the result of a promise', function(done){
+    var expected = {
+      results: []
+    };
+    var handler = function(){
+      return new Promise(function(resolve){
+        resolve(expected);
+      });
+    };
+    var ctx = {
+      send: jasmine.createSpy()
+    };
+    var res = {
+      writable: true
+    };
+    var fn = wrapRoute(handler, ctx, ceres);
+    fn({}, res, function(){})
+      .then(function(){
+        expect(ctx.send).toHaveBeenCalledWith(expected);
+        done();
+      });
+  });
+
+  it('should not write the response if is not writable', function(done){
+    var expected = {
+      results: []
+    };
+    var handler = function(){
+      return new Promise(function(resolve){
+        resolve(expected);
+      });
+    };
+    var ctx = {
+      send: jasmine.createSpy()
+    };
+    var res = {
+      writable: false
+    };
+    var fn = wrapRoute(handler, ctx, ceres);
+    fn({}, res, function(){})
+      .then(function(){
+        expect(ctx.send).not.toHaveBeenCalled();
+        done();
+      });
+  });
+});
--- src/controllers/Router.js
+++ src/controllers/Router.js
@@ -1,11 +1,5 @@
 var express = require('express');
-var Promise = require('bluebird');
-
-/**
- * @file Automatically parse the routes option on a Controller.
- */
-var bindEach = require('../lib/bindAll');
-var Responses = require('./Responses');
+var wrapRoute = require('./wrapRoute');
 
 /**
  * Get the full path of a controller endpoint
@@ -15,103 +9,6 @@ var Responses = require('./Responses');
  */
 function getFullPath(controller, path) {
   return (controller.endpoint + path).replace('//','/');
-}
-
-/**
- * Wrap the logic and provide a new this context
- *
- * @param     {Function}    handler
- * @param     {Object}      ctx         `this` from created Controller
- * @return    {Express.route}
- */
-function wrapRoute(handler, ctx, ceres) {
-  return function(req, res, next) {
-    /**
-     * Create this context
-     *
-     * @type    {Object}
-     */
-    var context = Object.assign({
-      /**
-       * Make req available on this
-       * @type {Express.Request}
-       */
-      req: req,
-
-      /**
-       * Make res available on this
-       * @type {Express.Responses}
-       */
-      res: res,
-
-			/**
-			 * Make the next callback available so we can pass the errors along
-			 * @type    {Function}
-			 */
-			next: next,
-
-      /**
-       * Ceres config
-       * @type {Object}
-       */
-      config: ceres.config,
-
-      /**
-       * Make the loggere availabe to each request
-       * @type {Winston}
-       */
-      log: ceres.log,
-
-      /**
-       * Old way of accessing context
-       * @deprecated
-       * @type {Object}
-       */
-      controller: ctx,
-
-      /**
-       * All models
-       * @type {Object}
-       */
-      models: req.app.get('models')
-
-    }, ctx);
-
-    /**
-     * User overridden responses
-     * @type    {Oject}
-     */
-    var responses = Object.assign({}, Responses, ctx.responses);
-
-    /**
-     * Bind req and res to each response
-     */
-    responses = bindEach(responses, context);
-
-    // Attach to context
-    Object.assign(context, responses);
-
-    // Attempt to catch any errors and handle them gracefully
-    try {
-			var result = handler.call(context, req, res, next, ceres);
-
-			if (result instanceof Promise) {
-				// If we see a promise then try to send the body automatically
-				return result
-					.then(function(body){
-						// Make sure the request is writable and that we have something to send
-						if (res.writable) {
-							context.send(body);
-						} else {
-							ceres.log._ceres.debug('Unable to write body', body);
-						}
-					})
-					.catch(next);
-				}
-		} catch(err) {
-			next(err);
-		}
-  };
 }
 
 /**
