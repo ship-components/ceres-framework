386d09436f425476662c13a93a8f8e98d4d5488a
Isaac Suttell <isaac_suttell@playstation.sony.com>
2017-05-23 08:39:17 -0700

    moved sticky cluster to it's own option and setup non sticky clustering

--- src/setup/run-cluster.js
+++ src/setup/run-cluster.js
@@ -1,4 +1,4 @@
-var sticky = require('sticky-session');
+var cluster = require('cluster');
 var Promise = require('bluebird');
 
 var Server = require('./Server');
@@ -18,50 +18,53 @@ var logStartTime = require('../lib/logStartTime');
  * @return {Promise}
  */
 module.exports = function(ceres) {
-	// processManagement
-	return this
-		.connect.call(this, ceres)
-		.then(function listen() {
-			return new Promise(function(resolve, reject){
-				try {
-					// Setup express server
-					var server = Server.call(ceres, ceres);
+  // processManagement
+  return this
+    .connect.call(this, ceres)
+    .then(function listen() {
+      return new Promise(function(resolve, reject){
+        try {
 
-					if (!ceres.config.instances || ceres.config.instances === 1) {
-						// Skip sticky session setup if we only have a single instance. Allows
-						// for debugging
-						server.listen(ceres.config.port, function(){
-							logStartTime('Server took %ds to start listening', ceres);
-							ceres.log._ceres.info('Listening on %d (%s)', ceres.config.port, ceres.config.env);
-							resolve();
-						});
-						return;
-					}
+          if (!ceres.config.instances || ceres.config.instances === 1) {
+            // If we only have a single instance no need to run the cluster
+            Server.call(ceres, ceres).listen(ceres.config.port, function(){
+              logStartTime('Server took %ds to start listening', ceres);
+              ceres.log._ceres.info('Listening on %d (%s)', ceres.config.port, ceres.config.env);
+              resolve();
+            });
+            return;
+          }
 
-					// Start sticky session server which handles the cluster
-					var isChild = sticky.listen(server, ceres.config.port, {
-						workers: ceres.config.instances
-					});
+          if (cluster.isMaster) {
+            if (ceres.config.pid) {
+              // Setup Pid if we're configure
+              ceres.pid = new Pid(ceres.config.pid);
+              ceres.log._ceres.silly('pid %d written to %s', ceres.pid.id, ceres.pid.options.path);
+            }
 
-					if (!isChild && ceres.config.pid) {
-						// Setup Pid
-						ceres.pid = new Pid(ceres.config.pid);
-						ceres.log._ceres.silly('pid %d written to %s', ceres.pid.id, ceres.pid.options.path);
-					}
+            // Fork children
+            for (var i = 0; i < ceres.config.instances; i++) {
+              cluster.fork();
+            }
 
-					if (!isChild) {
-						server.once('listening', function(){
-							logStartTime('Master took %ds to start listening', ceres);
-							ceres.log._ceres.info('Listening on %d (%s)', ceres.config.port, ceres.config.env);
-							resolve();
-						});
-					} else {
-						logStartTime('Child ready after %ds', ceres);
-						resolve();
-					}
-				} catch(err) {
-					reject(err);
-				}
-			});
-		});
+            // Attempt to restart children that crash
+            cluster.on('exit', function(worker, code, signal){
+              ceres.log._ceres.error('%s exited with %s. Starting new worker...', worker.process.pid, code || signal);
+              cluster.fork();
+            });
+
+            ceres.log._ceres.info('Master spawned %d children', ceres.config.instances);
+          } else {
+            // Setup the server
+            Server.call(ceres, ceres).listen(ceres.config.port, function(){
+              logStartTime('Child ready after %ds', ceres);
+              ceres.log._ceres.info('Listening on %d (%s)', ceres.config.port, ceres.config.env);
+              resolve();
+            });
+          }
+        } catch(err) {
+          reject(err);
+        }
+      });
+    });
 };
